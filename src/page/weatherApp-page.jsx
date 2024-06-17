21be171228a123a7da399550960f5922d5e3c530
Onuora Ebube Ebuka <onuoraebube44@gmail.com>
2024-06-17 01:04:59 +0100

    Still making changes

--- src/page/weatherApp-page.jsx
+++ src/page/weatherApp-page.jsx
@@ -6,29 +6,88 @@ import rain_icon from "../assets/rain.png"
 import snow_icon from "../assets/snow.png"
 import humidity_icon from "../assets/humidity.png"
 import wind_icon from "../assets/wind.png"
+import { useEffect, useRef, useState } from "react"
 
 
 const WeatherApp = ()=>{
+
+    const inputRef = useRef();
+
+    const [weatherData, setWeatherData] = useState(false);
+
+    const allIcons = {
+        "01d": clear_icon,
+        "01n": clear_icon,
+        "02d": cloud_icon,
+        "02n": cloud_icon,
+        "03d": cloud_icon,
+        "03n":cloud_icon,
+        "04d":drizzle_icon,
+        "04n":drizzle_icon,
+        "09d": rain_icon,
+        "09n": rain_icon,
+        "10d": rain_icon,
+        "10n": rain_icon,
+        "13d": snow_icon,
+        "13n":snow_icon,
+    }
+
+    const search = async(city)=>{
+        if(city === ""){
+            alert("Enter City Name");
+            return;
+        }
+        try{
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_WEATHER_API_KEY}`
+
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if(!response.ok){
+                alert(data.message);
+                return;
+            }
+
+            console.log(data)
+            const icon = allIcons[data.weatherData[0].icon]  || clear_icon;
+            setWeatherData({
+                humidity: data.main.humidity, //from the console
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon
+            })
+        }catch(error){
+            setWeatherData(false)
+            console.error("Error in fetching weather data");
+        }
+    }
+
+    useEffect(() => {
+        search("Europe")
+    }, [])
     return(
         <section className="h-cover" >
             <div className="weather">
-                <div className="search-bar">
-                    <input type="text" placeholder="Search"/>
-                    <img src={search_icon} alt="Search Icon"/>
-                    
+           
+                 <div className="search-bar">
+                    <input ref={inputRef} type="text" placeholder="Search"/>
+                    <img src={search_icon} alt="Search Icon" onClick={() =>search(inputRef.current.value)}/> 
                 </div>
 
-                <img src={clear_icon} alt="clear Image" className="weather-icon"/>
-                <p className="temperature">16°C</p>
-                <p className="location">London</p>
+                {weatherData? <>
+
+                    <img src={weatherData.icon} alt="Weather Image" className="weather-icon"/>
+                <p className="temperature">{weatherData.temperature}°C</p>
+                <p className="location">{weatherData.location}</p>
 
                 <div className="weather-data">
                     <div className="col">
                         <img src={humidity_icon} alt="Image"/>
                         
                         <div>
-                                <p>91 %</p>
-                                <span>Humidity</span>
+                                <p>{weatherData.humidity} %</p>
+                                <strong>Humidity</strong>
                         </div>
                     </div>
 
@@ -36,11 +95,14 @@ const WeatherApp = ()=>{
                         <img src={wind_icon} alt="Image"/>
                         
                         <div>
-                                <p>3.6 Km/h</p>
-                                <span>Wind Speed</span>
+                                <p>{weatherData.windSpeed}Km/h</p>
+                                <strong>Wind Speed</strong>
                         </div>
                     </div>
-                </div>
+                
+            </div>
+                </>: <></>}
+                
             </div>
         </section>
     )
